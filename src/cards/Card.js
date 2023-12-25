6c12e2c421ef5ea4b276a83b14ca4f4503dca464
NoaTuson <noatu1999@gmail.com>
2023-12-26 01:00:58 +0200

    update

--- src/cards/Card.js
+++ src/cards/Card.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
@@ -11,8 +11,19 @@ import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 
+export default function MyCard({ card, toggleFavorite }) {
+    // Local state to handle favorite status
+    const [isFavorite, setIsFavorite] = useState(card.favorite);
 
-export default function MyCard({ card }) {
+    // This function is called when the favorite icon is clicked
+    const handleFavoriteClick = () => {
+        // Update the local UI immediately for a responsive feel
+        setIsFavorite(!isFavorite);
+
+        // Call the parent function to handle the API update
+        toggleFavorite(card.id, !isFavorite);
+    };
+
     return (
         <Card sx={{ maxWidth: 345 }}>
             <CardHeader
@@ -28,7 +39,7 @@ export default function MyCard({ card }) {
                 component="img"
                 height="194"
                 image={card.imgUrl}
-                alt="Paella dish"
+                alt={card.imgAlt}
             />
             <CardContent>
                 <Typography variant="body2" color="text.secondary">
@@ -36,8 +47,8 @@ export default function MyCard({ card }) {
                 </Typography>
             </CardContent>
             <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
-                    <FavoriteIcon />
+                <IconButton aria-label="add to favorites" onClick={handleFavoriteClick}>
+                    <FavoriteIcon color={isFavorite ? 'error' : 'action'} />
                 </IconButton>
                 <IconButton aria-label="share">
                     <ShareIcon />
@@ -46,3 +57,29 @@ export default function MyCard({ card }) {
         </Card>
     );
 }
+
+// Function that would be in the parent component of MyCard
+
+const toggleFavorite = (cardId, newFavoriteStatus) => {
+  // Here you'd make an API call to update the favorite status
+  fetch(`https://api.shipap.co.il/cards/${cardId}/favorite?token=7cddfc3e-a309-11ee-beec-14dda9d4a5f0`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      // Include other headers as necessary, such as authorization headers
+    },
+    body: JSON.stringify({ favorite: newFavoriteStatus }),
+    credentials: 'include',
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    // Handle the successful update here, possibly refreshing the list of cards or updating state
+  })
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+    // Revert the favorite status in the UI if the server update fails
+    setIsFavorite(!newFavoriteStatus);
+  });
+};
--- src/cards/FavCards.js
+++ src/cards/FavCards.js
@@ -1,8 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import Card from './Card'; // Assuming you have a Card component to display each card
+
 export default function FavCards() {
-    return (
+  const [favCards, setFavCards] = useState([]);
+
+  useEffect(() => {
+    const fetchFavCards = async () => {
+      try {
+        const response = await fetch('https://api.shipap.co.il/cards/favorite?token=7cddfc3e-a309-11ee-beec-14dda9d4a5f0', {
+          credentials: 'include',
+        });
+        const data = await response.json();
+        setFavCards(data);
+      } catch (error) {
+        console.error('Error fetching favorite cards:', error);
+      }
+    };
+
+    fetchFavCards();
+  }, []);
+
+  const addFavorite = async (cardId) => {
+    // Implementation for adding a favorite card
+  };
+
+  const removeFavorite = async (cardId) => {
+    // Implementation for removing a favorite card
+  };
+
+  return (
+    <div>
+      <h3 className="tempTitle">Favorite Cards</h3>
       <div>
-          <h3 className="tempTitle">כרטיסים מועדפים </h3>
+        {favCards.map((card) => (
+          <Card
+            key={card.id}
+            card={card}
+            onAddFavorite={() => addFavorite(card.id)}
+            onRemoveFavorite={() => removeFavorite(card.id)}
+          />
+        ))}
       </div>
-    )
-  }
-  
+    </div>
+  );
+}
