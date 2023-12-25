ec4112de826293b2b6a08b37bd959c871a6fe5ec
NoaTuson <noatu1999@gmail.com>
2023-12-26 01:25:49 +0200

    update

--- src/cards/Card.js
+++ src/cards/Card.js
@@ -11,18 +11,62 @@ import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 
-export default function MyCard({ card, toggleFavorite }) {
-    // Local state to handle favorite status
+export default function MyCard({ card, onFavoriteChange }) {
+
     const [isFavorite, setIsFavorite] = useState(card.favorite);
 
-    // This function is called when the favorite icon is clicked
-    const handleFavoriteClick = () => {
-        // Update the local UI immediately for a responsive feel
-        setIsFavorite(!isFavorite);
-
-        // Call the parent function to handle the API update
-        toggleFavorite(card.id, !isFavorite);
+ const addFavorite = () => {
+        fetch(`https://api.shipap.co.il/cards/${card.id}/favorite?token=7cddfc3e-a309-11ee-beec-14dda9d4a5f0`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                // Include other headers as necessary
+            },
+            credentials: 'include',
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            setIsFavorite(true);
+            onFavoriteChange(card.id, true); // Notify the parent component
+        })
+        .catch(error => {
+            console.error('There has been a problem with adding favorite:', error);
+        });
     };
+
+    // Function to remove the card from favorites
+    const removeFavorite = () => {
+        fetch(`https://api.shipap.co.il/cards/${card.id}/unfavorite?token=7cddfc3e-a309-11ee-beec-14dda9d4a5f0`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                // Include other headers as necessary
+            },
+            credentials: 'include',
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            setIsFavorite(false);
+            onFavoriteChange(card.id, false); // Notify the parent component
+        })
+        .catch(error => {
+            console.error('There has been a problem with removing favorite:', error);
+        });
+    };
+
+    // Function to handle favorite toggle
+    const toggleFavorite = () => {
+        if (isFavorite) {
+            removeFavorite();
+        } else {
+            addFavorite();
+        }
+    };
+
 
     return (
         <Card sx={{ maxWidth: 345 }}>
@@ -43,11 +87,11 @@ export default function MyCard({ card, toggleFavorite }) {
             />
             <CardContent>
                 <Typography variant="body2" color="text.secondary">
-                    {card.subtitle}
+                    {card.description}
                 </Typography>
             </CardContent>
             <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites" onClick={handleFavoriteClick}>
+                <IconButton aria-label="add to favorites" onClick={toggleFavorite}>
                     <FavoriteIcon color={isFavorite ? 'error' : 'action'} />
                 </IconButton>
                 <IconButton aria-label="share">
@@ -57,29 +101,3 @@ export default function MyCard({ card, toggleFavorite }) {
         </Card>
     );
 }
-
-// Function that would be in the parent component of MyCard
-
-const toggleFavorite = (cardId, newFavoriteStatus) => {
-  // Here you'd make an API call to update the favorite status
-  fetch(`https://api.shipap.co.il/cards/${cardId}/favorite?token=7cddfc3e-a309-11ee-beec-14dda9d4a5f0`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-      // Include other headers as necessary, such as authorization headers
-    },
-    body: JSON.stringify({ favorite: newFavoriteStatus }),
-    credentials: 'include',
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    // Handle the successful update here, possibly refreshing the list of cards or updating state
-  })
-  .catch(error => {
-    console.error('There has been a problem with your fetch operation:', error);
-    // Revert the favorite status in the UI if the server update fails
-    setIsFavorite(!newFavoriteStatus);
-  });
-};
